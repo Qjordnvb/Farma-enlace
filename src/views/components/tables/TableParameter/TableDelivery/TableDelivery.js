2012c06fae93f4f13d17820cdbd7a43afd513b1a
Jesus Escobar <leider924@gmail.com>
2022-08-13 14:05:21 -0500

    Replacement and Delivery tables connected

--- src/views/components/tables/TableParameter/TableDelivery/TableDelivery.js
+++ src/views/components/tables/TableParameter/TableDelivery/TableDelivery.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import {Table, Modal, Input, Form} from 'antd';
+import {Table, Modal, Form, Select} from 'antd';
 import './style.css';
 
 import btnAdd from '../../../../../assets/img/add.png';
 import {useCustomDelivery} from './hooks';
-const {Search} = Input;
+/*const {Search} = Input;*/
+const {Option} = Select;
 export default function TableDelivery() {
   const {
     dataSource,
-    setDataSource,
+
     columns,
     isAdd,
     addingFile,
     setAddingFile,
     resetAdd,
-    onAddFile
+    onAddFile,
+    reasonsList,
+    productsList,
+    onCreateDelivery
   } = useCustomDelivery();
   return (
     <div>
@@ -29,37 +33,49 @@ export default function TableDelivery() {
               resetAdd();
             }}
             onOk={() => {
-              setDataSource(() => {
-                return [...dataSource, addingFile];
-              });
-
+              onCreateDelivery();
               resetAdd();
             }}
           >
             <Form id="modalAdd">
               <Form.Item className="item-form" label="Motivos">
-                <Input
+                <Select
                   className="input-add"
-                  placeholder="000001"
-                  value={addingFile?.motivo}
+                  placeholder={'000001'}
+                  value={addingFile?.reason}
                   onChange={(e) => {
-                    setAddingFile(() => {
-                      return {motivo: e.target.value};
+                    setAddingFile({
+                      ...addingFile,
+                      reasonId: e
                     });
                   }}
-                />
+                >
+                  {reasonsList.map((reason) => {
+                    return (
+                      <Option key={reason.id} value={reason.id}>
+                        {reason.reason}
+                      </Option>
+                    );
+                  })}
+                </Select>
               </Form.Item>
               <Form.Item className="item-form" label="Uniformes">
-                <Search
+                <Select
                   className="input-add"
-                  placeholder="Seleccionar..."
-                  value={addingFile?.prendas}
+                  placeholder={'Seleccionar...'}
+                  value={addingFile?.uniformId}
                   onChange={(e) => {
-                    setAddingFile((pre) => {
-                      return {...pre, prendas: e.target.value};
-                    });
+                    setAddingFile({...addingFile, uniformId: e});
                   }}
-                />
+                >
+                  {productsList.map((product) => {
+                    return (
+                      <Option key={product.id} value={product.id}>
+                        {product.descripcion}
+                      </Option>
+                    );
+                  })}
+                </Select>
               </Form.Item>
             </Form>
           </Modal>
